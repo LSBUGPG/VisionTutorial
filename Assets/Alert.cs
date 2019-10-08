012db559703448ed37d40efd796cabfce97a4a69
Paul Sinnett <paul.sinnett@gmail.com>
2019-10-08 17:36:19 +0100

    Initial commit

---
+++ Assets/Alert.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Renderer))]
+public class Alert : MonoBehaviour
+{
+    public Material alertedMaterial;
+
+    void OnEnable()
+    {
+        Renderer boxRenderer = GetComponent<Renderer>();
+        boxRenderer.material = alertedMaterial;
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+}
---
+++ Assets/Vision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class Vision : MonoBehaviour
+{
+    public SphereCollider viewDistance;
+    public float fov = 90.0f;
+    public Behaviour alert;
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawFrustum(Vector3.zero, fov, viewDistance.radius, 0.5f, 1.0f);
+        Gizmos.DrawWireSphere(Vector3.zero, viewDistance.radius);
+    }
+
+    void OnTriggerStay(Collider thing)
+    {
+        if (thing.CompareTag("Player"))
+        {
+            Vector3 vectorToPlayer = thing.transform.position - transform.position;
+            if (Vector3.Angle(vectorToPlayer, transform.forward) < 0.5f * fov)
+            {
+                alert.enabled = true;
+            }
+        }
+    }    
+}
+
